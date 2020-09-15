c7a170840e7010518c98b6da76840eef1d153e39
Mit Gandhi <gmit816@gmail.com>
2020-09-15 00:35:26 -0700

    Recommendation section

--- js/main.js
+++ js/main.js
@@ -1,123 +1,139 @@
-/** 
+/**
  * ===================================================================
  * main js
  *
- * ------------------------------------------------------------------- 
- */ 
+ * -------------------------------------------------------------------
+ */
 
 (function($) {
 
-	"use strict";
+"use strict";
 
-	/*---------------------------------------------------- */
-	/* Preloader
-	------------------------------------------------------ */ 
+/*---------------------------------------------------- */
+/* Preloader
+------------------------------------------------------ */
    $(window).load(function() {
 
-      // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
+      // will first fade out the loading animation
+    $("#loader").fadeOut("slow", function(){
 
         // will fade out the whole DIV that covers the website.
         $("#preloader").delay(300).fadeOut("slow");
 
-      });       
+      });      
 
-  	})
-
-
-  	/*---------------------------------------------------- */
-  	/* FitText Settings
-  	------------------------------------------------------ */
-  	setTimeout(function() {
-
-   	$('#intro h1').fitText(1, { minFontSize: '42px', maxFontSize: '84px' });
-
-  	}, 100);
+  })
 
 
-	/*---------------------------------------------------- */
-	/* FitVids
-	------------------------------------------------------ */ 
-  	$(".fluid-video-wrapper").fitVids();
+  /*---------------------------------------------------- */
+  /* FitText Settings
+  ------------------------------------------------------ */
+  setTimeout(function() {
+
+    $('#intro h1').fitText(1, { minFontSize: '42px', maxFontSize: '84px' });
+
+  }, 100);
 
 
-	/*---------------------------------------------------- */
-	/* Owl Carousel
-	------------------------------------------------------ */ 
-	$("#owl-slider").owlCarousel({
+/*---------------------------------------------------- */
+/* FitVids
+------------------------------------------------------ */
+  $(".fluid-video-wrapper").fitVids();
+
+
+/*---------------------------------------------------- */
+/* Owl Carousel
+------------------------------------------------------ */
+$("#owl-slider").owlCarousel({
         navigation: false,
         pagination: true,
         itemsCustom : [
-	        [0, 1],
-	        [700, 2],
-	        [960, 3]
-	     ],
+       [0, 1],
+       [700, 2],
+       [960, 3]
+    ],
+        navigationText: false
+    });
+    $("#owl-slider2").owlCarousel({
+        navigation: false,
+        pagination: true,
+         loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+        itemsCustom : [
+       [0, 1],
+       [700, 1],
+       [960, 1]
+    ],
         navigationText: false
     });
 
 
-	/*----------------------------------------------------- */
-	/* Alert Boxes
-  	------------------------------------------------------- */
-	$('.alert-box').on('click', '.close', function() {
-	  $(this).parent().fadeOut(500);
-	});	
+
+/*----------------------------------------------------- */
+/* Alert Boxes
+  ------------------------------------------------------- */
+$('.alert-box').on('click', '.close', function() {
+ $(this).parent().fadeOut(500);
+});
 
 
-	/*----------------------------------------------------- */
-	/* Stat Counter
-  	------------------------------------------------------- */
+/*----------------------------------------------------- */
+/* Stat Counter
+  ------------------------------------------------------- */
    var statSection = $("#stats"),
        stats = $(".stat-count");
 
    statSection.waypoint({
 
-   	handler: function(direction) {
+    handler: function(direction) {
 
-      	if (direction === "down") {       		
+      if (direction === "down") {      
 
-			   stats.each(function () {
-				   var $this = $(this);
+  stats.each(function () {
+  var $this = $(this);
 
-				   $({ Counter: 0 }).animate({ Counter: $this.text() }, {
-				   	duration: 4000,
-				   	easing: 'swing',
-				   	step: function (curValue) {
-				      	$this.text(Math.ceil(curValue));
-				    	}
-				  	});
-				});
+  $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+  duration: 4000,
+  easing: 'swing',
+  step: function (curValue) {
+      $this.text(Math.ceil(curValue));
+    }
+  });
+});
 
-       	} 
+        }
 
-       	// trigger once only
-       	this.destroy();      	
+        // trigger once only
+        this.destroy();      
 
-		},
-			
-		offset: "90%"
-	
-	});	
+},
 
+offset: "90%"
 
-	/*---------------------------------------------------- */
-	/*	Masonry
-	------------------------------------------------------ */
-	var containerProjects = $('#folio-wrapper');
-
-	containerProjects.imagesLoaded( function() {
-
-		containerProjects.masonry( {		  
-		  	itemSelector: '.folio-item',
-		  	resize: true 
-		});
-
-	});
+});
 
 
-	/*----------------------------------------------------*/
-	/*	Modal Popup
-	------------------------------------------------------*/
+/*---------------------------------------------------- */
+/* Masonry
+------------------------------------------------------ */
+var containerProjects = $('#folio-wrapper');
+
+containerProjects.imagesLoaded( function() {
+
+containerProjects.masonry( {  
+  itemSelector: '.folio-item',
+  resize: true
+});
+
+});
+
+
+/*----------------------------------------------------*/
+/* Modal Popup
+------------------------------------------------------*/
    $('.item-wrap a').magnificPopup({
 
       type:'inline',
@@ -129,17 +145,17 @@
    });
 
    $(document).on('click', '.popup-modal-dismiss', function (e) {
-   	//e.preventDefault();
-   	$('#media').children('iframe').attr('src', '');
-   	// $('#media').each(function(){
-   	// 	this.player.pause();
-   	// });
-   	$.magnificPopup.close();
+    //e.preventDefault();
+    $('#media').children('iframe').attr('src', '');
+    // $('#media').each(function(){
+    // this.player.pause();
+    // });
+    $.magnificPopup.close();
    });
 
-	
-	/*-----------------------------------------------------*/
-  	/* Navigation Menu
+
+/*-----------------------------------------------------*/
+  /* Navigation Menu
    ------------------------------------------------------ */  
    var toggleButton = $('.menu-toggle'),
        nav = $('.main-navigation');
@@ -147,149 +163,149 @@
    // toggle button
    toggleButton.on('click', function(e) {
 
-		e.preventDefault();
-		toggleButton.toggleClass('is-clicked');
-		nav.slideToggle();
+e.preventDefault();
+toggleButton.toggleClass('is-clicked');
+nav.slideToggle();
 
-	});
+});
 
    // nav items
-  	nav.find('li a').on("click", function() {   
+  nav.find('li a').on("click", function() {  
 
-   	// update the toggle button 		
-   	toggleButton.toggleClass('is-clicked'); 
-   	// fadeout the navigation panel
-   	nav.fadeOut();   		
-   	     
-  	});
+    // update the toggle button
+    toggleButton.toggleClass('is-clicked');
+    // fadeout the navigation panel
+    nav.fadeOut();  
+       
+  });
 
 
    /*---------------------------------------------------- */
-  	/* Highlight the current section in the navigation bar
-  	------------------------------------------------------ */
-	var sections = $("section"),
-	navigation_links = $("#main-nav-wrap li a");	
+  /* Highlight the current section in the navigation bar
+  ------------------------------------------------------ */
+var sections = $("section"),
+navigation_links = $("#main-nav-wrap li a");
 
-	sections.waypoint( {
+sections.waypoint( {
 
        handler: function(direction) {
 
-		   var active_section;
+  var active_section;
 
-			active_section = $('section#' + this.element.id);
+active_section = $('section#' + this.element.id);
 
-			if (direction === "up") active_section = active_section.prev();
+if (direction === "up") active_section = active_section.prev();
 
-			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');			
+var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');
 
          navigation_links.parent().removeClass("current");
-			active_link.parent().addClass("current");
+active_link.parent().addClass("current");
 
-		}, 
+},
 
-		offset: '25%'
-	});
+offset: '25%'
+});
 
 
-	/*---------------------------------------------------- */
-  	/* Smooth Scrolling
-  	------------------------------------------------------ */
-  	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
+/*---------------------------------------------------- */
+  /* Smooth Scrolling
+  ------------------------------------------------------ */
+  $('.smoothscroll').on('click', function (e) {
 
-   		var target = this.hash,
-    	$target = $(target);
+e.preventDefault();
 
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
+    var target = this.hash,
+    $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
       }, 800, 'swing', function () {
-      	window.location.hash = target;
+      window.location.hash = target;
       });
 
-  	});  
-  
+  });  
+ 
 
    /*---------------------------------------------------- */
-	/*  Placeholder Plugin Settings
-	------------------------------------------------------ */ 
-	$('input, textarea, select').placeholder()  
+/*  Placeholder Plugin Settings
+------------------------------------------------------ */
+$('input, textarea, select').placeholder()  
 
 
-  	/*---------------------------------------------------- */
-	/*	contact form
-	------------------------------------------------------ */
+  /*---------------------------------------------------- */
+/* contact form
+------------------------------------------------------ */
 
-	/* local validation */
-	$('#contactForm').validate({
+/* local validation */
+$('#contactForm').validate({
 
-		/* submit via ajax */
-		submitHandler: function(form) {
+/* submit via ajax */
+submitHandler: function(form) {
 
-			var sLoader = $('#submit-loader');
+var sLoader = $('#submit-loader');
 
-			$.ajax({      	
+$.ajax({      
 
-		      type: "POST",
-		      url: "inc/sendEmail.php",
-		      data: $(form).serialize(),
-		      beforeSend: function() { 
+     type: "POST",
+     url: "inc/sendEmail.php",
+     data: $(form).serialize(),
+     beforeSend: function() {
 
-		      	sLoader.fadeIn(); 
+      sLoader.fadeIn();
 
-		      },
-		      success: function(msg) {
+     },
+     success: function(msg) {
 
-	            // Message was sent
-	            if (msg == 'OK') {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').hide();
-	               $('#contactForm').fadeOut();
-	               $('#message-success').fadeIn();   
-	            }
-	            // There was an error
-	            else {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').html(msg);
-		            $('#message-warning').fadeIn();
-	            }
+           // Message was sent
+           if (msg == 'OK') {
+            sLoader.fadeOut();
+              $('#message-warning').hide();
+              $('#contactForm').fadeOut();
+              $('#message-success').fadeIn();  
+           }
+           // There was an error
+           else {
+            sLoader.fadeOut();
+              $('#message-warning').html(msg);
+           $('#message-warning').fadeIn();
+           }
 
-		      },
-		      error: function() {
+     },
+     error: function() {
 
-		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
-		         $('#message-warning').fadeIn();
+      sLoader.fadeOut();
+      $('#message-warning').html("Something went wrong. Please try again.");
+        $('#message-warning').fadeIn();
 
-		      }
+     }
 
-	      });     		
-  		}
+     });    
+  }
 
-	});
+});
 
 
- 	/*----------------------------------------------------- */
-  	/* Back to top
-   ------------------------------------------------------- */ 
-	var pxShow = 300; // height on which the button will show
-	var fadeInTime = 400; // how slow/fast you want the button to show
-	var fadeOutTime = 400; // how slow/fast you want the button to hide
-	var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+  /*----------------------------------------------------- */
+  /* Back to top
+   ------------------------------------------------------- */
+var pxShow = 300; // height on which the button will show
+var fadeInTime = 400; // how slow/fast you want the button to show
+var fadeOutTime = 400; // how slow/fast you want the button to hide
+var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
    // Show or hide the sticky footer button
-	jQuery(window).scroll(function() {
+jQuery(window).scroll(function() {
 
-		if (!( $("#header-search").hasClass('is-visible'))) {
+if (!( $("#header-search").hasClass('is-visible'))) {
 
-			if (jQuery(window).scrollTop() >= pxShow) {
-				jQuery("#go-top").fadeIn(fadeInTime);
-			} else {
-				jQuery("#go-top").fadeOut(fadeOutTime);
-			}
+if (jQuery(window).scrollTop() >= pxShow) {
+jQuery("#go-top").fadeIn(fadeInTime);
+} else {
+jQuery("#go-top").fadeOut(fadeOutTime);
+}
 
-		}		
+}
 
-	});		
+});
 
 })(jQuery);
